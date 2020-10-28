267f19be39b78c0eef6e0e7785266055f2862952
김현우 <hyun0310woo@gmail.com>
2020-10-28 21:50:07 +0900

    Remove All and Fix New Home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import Education from "./components/Education";
-import Introduce from "./components/Introduce";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Contact from "./components/Contact";
-import PortfolioBar from "./components/PortfolioBar";
 import Home from "./components/Home";
 import "./App.css";
 
 const App = () => {
-  return (
-    <div id="colorlib-page">
-      <PortfolioBar />
-      <Route exact path="/" component={Home} />
-      <Route path="/Education" component={Education} />
-      <Route path="/Projects" component={Projects} />
-      <Route path="/Skills" component={Skills} />
-      <Route path="/Contact" component={Contact} />
-    </div>
-  );
+  return <Home />;
 };
 
 export default App;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,11 +2,7 @@ import React from "react";
 import "../componentsStyles/Projects.css";
 
 const Projects = () => {
-  return (
-    <div id="ProjectsCard">
-      <div id="ProjectsText">Projects</div>
-    </div>
-  );
+  return <div>프로젝</div>;
 };
 
 export default Projects;
